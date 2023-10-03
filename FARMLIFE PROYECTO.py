6b0784c87554ac538eea9b8d420b7dc3aa1acac0
Julio Cáceres <125315663+Julio-Caceres1045@users.noreply.github.com>
2023-10-02 19:17:16 -0600

    Update FARMLIFE PROYECTO.py
    Añadir login

--- FARMLIFE PROYECTO.py
+++ FARMLIFE PROYECTO.py
@@ -1,1 +1,14 @@
+usuarios = {}
 
+def registrar_usuario():
+    print("Registro de nuevo usuario")
+    usuario = input("Elija un nombre de usuario: ")
+
+    # Verificar si el usuario ya existe
+    if usuario in usuarios:
+        print("El usuario ya existe. Por favor, elija otro nombre de usuario.")
+        return
+
+    contrasena = input("Elija una contraseña: ")
+    usuarios[usuario] = contrasena
+    print("Registro exitoso. Ahora puede iniciar sesión con su nuevo usuario.")
