bcd01eb2560a821890645a05877bec8fe99c367c
Iván Ordoñez <124651796+IvanOrdonez22@users.noreply.github.com>
2023-10-02 19:19:36 -0600

    Update FARMLIFE PROYECTO.py
    Ingreso de registro

--- FARMLIFE PROYECTO.py
+++ FARMLIFE PROYECTO.py
@@ -3,8 +3,7 @@ usuarios = {}
 def registrar_usuario():
     print("Registro de nuevo usuario")
     usuario = input("Elija un nombre de usuario: ")
-
-    # Verificar si el usuario ya existe
+    
     if usuario in usuarios:
         print("El usuario ya existe. Por favor, elija otro nombre de usuario.")
         return
@@ -12,3 +11,12 @@ def registrar_usuario():
     contrasena = input("Elija una contraseña: ")
     usuarios[usuario] = contrasena
     print("Registro exitoso. Ahora puede iniciar sesión con su nuevo usuario.")
+
+def login():
+    intentos = 3
+    while intentos > 0:
+        usuario = input("Usuario: ")
+        contrasena = input("Contraseña: ")
+
+        if usuario in usuarios and usuarios[usuario] == contrasena:
+            print("Inicio de sesión exitoso. ¡Bienvenido,", usuario, "!")
